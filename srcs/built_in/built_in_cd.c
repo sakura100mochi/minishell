e8f0cf0849a53a9ee332ec223bef07ab6e1f8355
deco-L <scotchwitchbroom@gmail.com>
2023-09-20 14:36:34 +0900

    tmp cd

--- srcs/built_in/built_in_cd.c
+++ srcs/built_in/built_in_cd.c
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 05:36:22 by csakamot          #+#    #+#             */
-/*   Updated: 2023/09/20 13:57:15 by csakamot         ###   ########.fr       */
+/*   Updated: 2023/09/20 14:33:35 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,31 +31,43 @@ static t_env	*serch_env_variable(t_env *head, char *variable)
 	return (serch_variable);
 }
 
-static t_env	*set_oldpwd(t_env *env_variable)
+static t_env	*set_pwd(t_env *env_variable, char *head_variable)
 {
 	char	*wd_path;
 	char	*variable;
 
 	wd_path = NULL;
 	wd_path = getcwd(wd_path, PATH_MAX);
-	variable = ft_strjoin("OLDPWD=", wd_path);
+	variable = ft_strjoin(head_variable, wd_path);
 	free(env_variable->variable);
 	env_variable->variable = variable;
 	free(wd_path);
 	return (env_variable);
 }
 
-static void	charnge_derectory(t_parser *parser)
+static void	change_directory(t_parser *parser)
 {
 	size_t	len;
 	char	*wd_path;
+	char	*tmp_path;
 	char	*absolute_path;
 
 	if (!*(parser)->str)
+	{
 		chdir("~/");
+		return ;
+	}
+	len = ft_strlen(parser->str);
+	if (len == 1 && ft_strncmp(parser->str, "-", 1))
+		return ;
 	wd_path = NULL;
 	wd_path = getcwd(wd_path, PATH_MAX);
-	return ;
+	tmp_path = ft_strjoin(wd_path, "/");
+	absolute_path = ft_strjoin(tmp_path, parser->str);
+	chdir(absolute_path);
+	free(wd_path);
+	free(tmp_path);
+	free(absolute_path);
 }
 
 t_env	*built_in_cd(t_env *env_variable, t_parser *parser)
@@ -65,8 +77,10 @@ t_env	*built_in_cd(t_env *env_variable, t_parser *parser)
 	head = env_variable;
 	env_variable = serch_env_variable(head, "OLDPWD=");
 	if (env_variable != head)
-		env_variable = set_oldpwd(env_variable);
+		env_variable = set_pwd(env_variable, "OLDPWD=");
 	change_directory(parser);
-	ft_printf("cd:Not yet implemented.\n");
-	return (env_variable);
+	env_variable = serch_env_variable(head, "PWD=");
+	if (env_variable != head)
+		env_variable = set_pwd(env_variable, "PWD=");
+	return (head);
 }
--- srcs/built_in/built_in_exit.c
+++ srcs/built_in/built_in_exit.c
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 05:36:15 by csakamot          #+#    #+#             */
-/*   Updated: 2023/09/20 11:48:36 by csakamot         ###   ########.fr       */
+/*   Updated: 2023/09/20 14:34:00 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	built_in_exit(t_init *state)
 {
-	ft_printf("exit:Not yet implemented.\n");
 	double_array_free(state->exe->command);
 	free(state->prompt);
 	delete_all_env_node(state->env);
