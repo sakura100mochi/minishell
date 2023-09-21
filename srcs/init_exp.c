7ab119d09cad2db3290cef79b29c78737e310889
deco-L <scotchwitchbroom@gmail.com>
2023-09-21 11:12:17 +0900

    export revision

--- srcs/init_exp.c
+++ srcs/init_exp.c
@@ -6,11 +6,40 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 09:37:18 by csakamot          #+#    #+#             */
-/*   Updated: 2023/09/21 09:54:33 by csakamot         ###   ########.fr       */
+/*   Updated: 2023/09/21 11:10:34 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+static char	*exp_variable_format(char *variable)
+{
+	int			flag;
+	size_t		i;
+	size_t		len;
+	char		*tmp_str;
+	char		*result;
+
+	flag = 0;
+	i = 0;
+	len = ft_strlen(variable) + 2;
+	tmp_str = (char *)ft_calloc(sizeof(char), (len + 1));
+	while (i++ < len - 1)
+	{
+		if (!flag && i - 1 > 0 && tmp_str[i - 2] == '=')
+		{
+			tmp_str[i - 1] = '"';
+			flag++;
+		}
+		else
+			tmp_str[i - 1] = variable[i - 1 - flag];
+	}
+	tmp_str[i - 1] = '"';
+	tmp_str[i] = '\0';
+	result = ft_strjoin("declare -x ", tmp_str);
+	free(tmp_str);
+	return (result);
+}
 
 void	exp_nodeadd_back(t_exp **env, t_exp *new)
 {
@@ -48,7 +77,7 @@ t_init	*init_exp(t_init *state)
 	start = exp_variable;
 	while (environ[index])
 	{
-		new = new_exp_node(ft_strjoin("dexlare -x ", environ[index]), 0);
+		new = new_exp_node(exp_variable_format(environ[index]), 0);
 		exp_nodeadd_back(&exp_variable, new);
 		index++;
 	}
