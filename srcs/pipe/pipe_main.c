1e9fec04d04299d96a36f3bf26ad4c0465ed89ef
deco-L <scotchwitchbroom@gmail.com>
2023-09-26 16:23:46 +0900

    pipe example

--- srcs/pipe/pipe_main.c
+++ srcs/pipe/pipe_main.c
@@ -6,13 +6,52 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 13:06:30 by csakamot          #+#    #+#             */
-/*   Updated: 2023/09/26 16:09:58 by csakamot         ###   ########.fr       */
+/*   Updated: 2023/09/26 16:22:57 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/pipe.h"
 
-int	main(void)
-{
-	return (0);
-}
+
+// int main() {
+//     int pipe_fd[2];
+//     pid_t child_pid;
+
+//     // パイプを作成
+//     if (pipe(pipe_fd) == -1) {
+//         perror("pipe");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     // 子プロセスをフォーク
+//     child_pid = fork();
+
+//     if (child_pid == -1) {
+//         perror("fork");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     if (child_pid == 0) { // 子プロセス
+//         // パイプの出力側を標準出力にリダイレクト
+//         dup2(pipe_fd[1], STDOUT_FILENO);
+//         close(pipe_fd[0]);
+//         close(pipe_fd[1]);
+
+//         // /bin/ls コマンドを実行
+//         execl("/bin/ls", "/bin/ls", NULL);
+//         perror("execl");
+//         exit(EXIT_FAILURE);
+//     } else { // 親プロセス
+//         // パイプの入力側を標準入力にリダイレクト
+//         dup2(pipe_fd[0], STDIN_FILENO);
+//         close(pipe_fd[0]);
+//         close(pipe_fd[1]);
+
+//         // /usr/bin/grep コマンドを実行
+//         execl("/usr/bin/grep", "/usr/bin/grep", "Make", NULL);
+//         perror("execl");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     return 0;
+// }
