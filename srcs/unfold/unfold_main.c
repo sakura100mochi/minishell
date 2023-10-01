3e5e73b4f8394f2e5066ee604023f8b8ead2737b
deco-L <scotchwitchbroom@gmail.com>
2023-10-01 13:15:55 +0900

    1

--- srcs/unfold/unfold_main.c
+++ srcs/unfold/unfold_main.c
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 16:16:53 by csakamot          #+#    #+#             */
-/*   Updated: 2023/09/30 20:11:58 by csakamot         ###   ########.fr       */
+/*   Updated: 2023/10/01 13:15:13 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,27 +20,36 @@ static char	*unfold_variable(t_env *env, char *str)
 	return (NULL);
 }
 
-static int	find_env_variable(char *str)
+static size_t	find_env_variable(char *str)
 {
 	size_t	index;
+	size_t	nbr;
 
 	index = 0;
+	nbr = 0;
 	while (str[index] != '\0')
 	{
-		if (str[index] == '$')
-			return (0);
+		if (str[index] == '$' && str[index + 1] != '\0')
+		{
+			nbr++;
+			index++;
+		}
 		index++;
 	}
+	if (nbr)
+		return (0);
 	return (1);
 }
 
 int	unfold_main(t_env *env, t_parser *parser, char *file)
 {
-	(void)parser;
-	(void)file;
-	(void)file;
-	if (!find_env_variable(parser->cmd))
+	char	**strage;
+	size_t	len;
+
+	len = find_env_variable(parser->cmd);
+	if (!len)
 	{
+		strage = (char **)ft_calloc(sizeof(char *), len + 1);
 		unfold_variable(env, *parser->cmd);
 		return ;
 	}
