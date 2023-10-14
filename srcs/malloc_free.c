9344b2f44eecde619915277cf453136486ce4224
deco-L <scotchwitchbroom@gmail.com>
2023-10-14 15:40:20 +0900

    1

--- srcs/malloc_free.c
+++ srcs/malloc_free.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   free.c                                             :+:      :+:    :+:   */
+/*   malloc_free.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yhirai <yhirai@student.42.fr>              +#+  +:+       +#+        */
+/*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 18:29:12 by yhirai            #+#    #+#             */
-/*   Updated: 2023/10/13 15:02:19 by yhirai           ###   ########.fr       */
+/*   Updated: 2023/10/14 15:39:45 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,12 +54,15 @@ void	double_array_free(char **array)
 	size_t	index;
 
 	index = 0;
-	while (array[index] != NULL)
+	if (array != NULL)
 	{
-		free(array[index]);
-		index++;
+		while (array[index] != NULL)
+		{
+			free(array[index]);
+			index++;
+		}
+		free(array);
 	}
-	free(array);
 	return ;
 }
 
@@ -69,20 +72,24 @@ void	delete_all_env_node(t_env *env_variable)
 	t_env	*current_location;
 
 	start = env_variable;
-	if (env_variable -> next == env_variable)
+	if (env_variable == NULL)
 	{
-		free(env_variable);
-		return ;
+		if (env_variable -> next == env_variable)
+		{
+			free(env_variable);
+			return ;
+		}
+		env_variable = env_variable -> next;
+		while (!(env_variable -> head))
+		{
+			if (env_variable != NULL)
+				free(env_variable->variable);
+			current_location = env_variable;
+			env_variable = (env_variable -> next);
+			free(current_location);
+		}
+		free(start);
 	}
-	env_variable = env_variable -> next;
-	while (!(env_variable -> head))
-	{
-		free(env_variable->variable);
-		current_location = env_variable;
-		env_variable = (env_variable -> next);
-		free(current_location);
-	}
-	free(start);
 	return ;
 }
 
@@ -92,19 +99,23 @@ void	delete_all_exp_node(t_exp *exp_variable)
 	t_exp	*current_location;
 
 	start = exp_variable;
-	if (exp_variable -> next == exp_variable)
+	if (exp_variable != NULL)
 	{
-		free(exp_variable);
-		return ;
+		if (exp_variable -> next == exp_variable)
+		{
+			free(exp_variable);
+			return ;
+		}
+		exp_variable = exp_variable -> next;
+		while (!(exp_variable -> head))
+		{
+			if (exp_variable != NULL)
+				free(exp_variable->variable);
+			current_location = exp_variable;
+			exp_variable = (exp_variable -> next);
+			free(current_location);
+		}
+		free(start);
 	}
-	exp_variable = exp_variable -> next;
-	while (!(exp_variable -> head))
-	{
-		free(exp_variable->variable);
-		current_location = exp_variable;
-		exp_variable = (exp_variable -> next);
-		free(current_location);
-	}
-	free(start);
 	return ;
 }
