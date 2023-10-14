c6434dc6b85f03b2e627660e205106c422fa7c60
deco-L <scotchwitchbroom@gmail.com>
2023-10-14 15:02:01 +0900

    1

--- srcs/built_in/built_in.c
+++ srcs/built_in/built_in.c
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 05:18:37 by csakamot          #+#    #+#             */
-/*   Updated: 2023/10/14 15:00:47 by csakamot         ###   ########.fr       */
+/*   Updated: 2023/10/14 15:01:27 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,11 +36,11 @@ static int	do_built_in(t_data *data, t_parser *parser, char *file)
 	return (YES);
 }
 
-static int	do_dup_built_in(t_data *data, t_file *file, int fd, char *str)
-{
-	int		status;
-	pid_t	pid;
-
+// static int	do_dup_built_in(t_data *data, t_file *file, int fd, char *str)
+// {
+// 	int		status;
+// 	pid_t	pid;
+// 
 // 	backup = 0;
 // 	if (file->type == OUTPUT || file->type == APPEND)
 // 	{
@@ -60,11 +60,11 @@ static int	do_dup_built_in(t_data *data, t_file *file, int fd, char *str)
 // 	return (NO);
 // }
 
-static void	check_fd(t_data *data, t_file *file, char *str)
-{
-	t_file	*head;
-	int		fd;
-
+// static void	check_fd(t_data *data, t_file *file, char *str)
+// {
+// 	t_file	*head;
+// 	int		fd;
+// 
 // 	head = file;
 // 	if (str == NULL)
 // 		return ;
