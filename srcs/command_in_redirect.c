23ccefdd994e77e74a492271e9cad3b4574f6e57
deco-L <scotchwitchbroom@gmail.com>
2023-10-17 20:12:23 +0900

    norminette

--- srcs/command_in_redirect.c
+++ srcs/command_in_redirect.c
@@ -6,51 +6,12 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 11:40:15 by csakamot          #+#    #+#             */
-/*   Updated: 2023/10/17 11:15:36 by csakamot         ###   ########.fr       */
+/*   Updated: 2023/10/17 20:10:16 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #include "../includes/built_in.h"
-
-// static int	do_dup_command(t_data *data, t_parser *parser,
-// 									t_file *file, char *str)
-// {
-// 	int	stdout;
-// 	int	stdin;
-// 	int	input;
-// 	int	output;
-
-// 	stdout = 0;
-// 	stdin = 0;
-// 	input = last_input_fd(file);
-// 	output = last_output_fd(file);
-// 	if (output)
-// 	{
-// 		stdout = dup(1);
-// 		dup2(output, STDOUT_FILENO);
-// 		close(output);
-// 	}
-// 	if (input)
-// 	{
-// 		stdin = dup(0);
-// 		dup2(input, STDIN_FILENO);
-// 		close(input);
-// 	}
-// 	if (!judge_built_in(data, parser, str))
-// 		fork_and_execve(data, data->exe, parser, str);
-// 	if (input)
-// 	{
-// 		dup2(stdin, STDIN_FILENO);
-// 		close(stdin);
-// 	}
-// 	if (output)
-// 	{
-// 		dup2(stdout, STDOUT_FILENO);
-// 		close(stdout);
-// 	}
-// 	return (YES);
-// }
 
 void	close_fd(t_file *file)
 {
@@ -72,41 +33,53 @@ int	check_redirect(t_parser *parser)
 	return (NO);
 }
 
-int	dup_command(t_data *data, t_parser *parser, t_file *file, char *str)
+static int	change_std_to_fd(int *stdin, int *stdout, int input, int output)
 {
-	int	stdout;
-	int	stdin;
-	int	input;
-	int	output;
-
-	stdout = 0;
-	stdin = 0;
-	input = last_input_fd(file);
-	output = last_output_fd(file);
 	if (output)
 	{
-		stdout = dup(1);
+		*stdout = dup(1);
 		dup2(output, STDOUT_FILENO);
 		close(output);
 	}
 	if (input)
 	{
-		stdin = dup(0);
+		*stdin = dup(0);
 		dup2(input, STDIN_FILENO);
 		close(input);
 	}
-	if (!judge_built_in(data, parser, str))
-		fork_and_execve(data, data->exe, parser, str);
+	return (YES);
+}
+
+static int	change_fd_to_std(int *stdin, int *stdout, int input, int output)
+{
 	if (input)
 	{
-		dup2(stdin, STDIN_FILENO);
-		close(stdin);
+		dup2(*stdin, STDIN_FILENO);
+		close(*stdin);
 	}
 	if (output)
 	{
-		dup2(stdout, STDOUT_FILENO);
-		close(stdout);
+		dup2(*stdout, STDOUT_FILENO);
+		close(*stdout);
 	}
+	return (YES);
+}
+
+int	dup_command(t_data *data, t_parser *parser, t_file *file, char *str)
+{
+	int	stdin;
+	int	stdout;
+	int	input;
+	int	output;
+
+	stdin = 0;
+	stdout = 0;
+	input = last_input_fd(file);
+	output = last_output_fd(file);
+	change_std_to_fd(&stdin, &stdout, input, output);
+	if (!judge_built_in(data, parser, str))
+		fork_and_execve(data, data->exe, parser, str);
+	change_fd_to_std(&stdin, &stdout, input, output);
 	close_fd(file);
 	return (YES);
 }
